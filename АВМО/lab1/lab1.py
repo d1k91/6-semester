c9820316c2178d5f53694f0ab6b0199122a390fe
Vadim Glinskiy <v.glinskiy@.com>
2025-02-19 21:27:45 +0700

    add some changes

--- АВМО/lab1/lab1.py
+++ АВМО/lab1/lab1.py
@@ -18,58 +18,75 @@ def check(arr):
     if all(x == 0 for x in arr):
         return 0
     
-def swap_max_in_column(matrix, matrix2, i):
+def swap_max_in_column(matrix, matrix2, i, col):
     max_value = -100000
     index = -1
     for k in range(i, len(matrix)):
-        if matrix[k][i] > max_value:
-            max_value = matrix[k][i]
+        if matrix[k][col] > max_value:
+            max_value = matrix[k][col]
             index = k
     
     if index != -1:
         matrix[i], matrix[index] = matrix[index], matrix[i]
         matrix2[i], matrix2[index] = matrix2[index], matrix2[i]
 
+
 def Jordan_Gauss(matrix):
     matrix2 = copy.deepcopy(matrix)
-    for i in range(len(matrix)):
+    n = len(matrix)
+    m = len(matrix[0]) - 1
+
+    for i in range(n):
         for r in range(len(matrix)):
-            ch = check(matrix[r])
+            ch = check(matrix2[r])
             if ch == -1:
-                return -1, matrix
+                return -1, matrix2
             if ch == 0:
-                return 0, matrix
+                return 0, matrix2
+        col = i
+        while col < m:
+            skip_column = True
+            for k in range(i, n):
+                if matrix[k][col] != 0:
+                    skip_column = False
+                    break
             
-        printarr(matrix)
+            if skip_column:
+                col += 1
+            else:
+                break
+
+        if col >= m:
+            break
+        
+        printarr(matrix2)
         print(' ')
+        
 
-        for k in range(i, len(matrix)):
-            swap_max_in_column(matrix, matrix2, i)   
+        swap_max_in_column(matrix, matrix2, i, col)
 
-        circ = matrix[i][i]
+        printarr(matrix2)
+        print(' ')
+        
+
+        circ = matrix[i][col]
         if circ == 0:
             continue
 
-        for j in range(len(matrix)):
-            if j != i:
-                matrix2[j][i] = 0
-        for k in range(i,len(matrix2[i])):
-            matrix2[i][k] = f.frac(matrix2[i][k], circ)
-        
-        printarr(matrix2)
-        print(' ')
+        for j in range(col, len(matrix[i])):
+            matrix2[i][j] = f.frac(matrix2[i][j], circ)
 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[r])):
-                if r != i and c > i:
-                    matrix2[r][c] = matrix[r][c] - f.frac((matrix[i][c]*matrix[r][i]),circ)
+        for r in range(n):
+            if r != i:
+                factor = matrix[r][col]
+                for c in range(col, len(matrix[r])):
+                    matrix2[r][c] = matrix[r][c] - f.frac((matrix[i][c] * factor), circ)
                     print(f'{matrix[r][c]} -> {matrix[r][c]} - ({matrix[i][c]}*{matrix[r][i]})/{circ} = {matrix[r][c] - f.frac((matrix[i][c]*matrix[r][i]),circ)}')
+                print(' ')
 
-            print(' ')
-        print(' ')
         printarr(matrix2)
-        print('---------------'*(len(matrix)+2))
-        
+        print('---------------' * (len(matrix) + 2))
+
         for r in range(len(matrix)):
             ch = check(matrix2[r])
             if ch == -1:
@@ -78,47 +95,54 @@ def Jordan_Gauss(matrix):
                 return 0, matrix2
 
         matrix = copy.deepcopy(matrix2)
+
     return 1, matrix
 
 def solve(key, mat):
-    match key:
-        case 0:
-            print("Система имеет бесконечно много решений")
-            #mat = [x for x in mat if check(x) != 0]
-            for i in range(len(mat)):
-                if mat[i][i] == 1:
-                    print(f'x{i+1} = ', end='')
-                for j in range(len(mat[i])):
-                    if j != i and j != len(mat[i])-1:
-                        if mat[i][j]<0:
-                            print(f' + {abs(mat[i][j])}*x{j+1}', end='')
-                        elif mat[i][j]>0:
-                            print(f' - {mat[i][j]}*x{j+1}',end='')
-                    elif j == len(mat[i])-1:
-                        if mat[i][j]<0:
-                            print(f' {mat[i][j]}',end='')
-                        elif mat[i][j]>0:
-                            print(f' + {abs(mat[i][j])}',end='')
-                print(' ')
-        
-        case -1:
-            print('Система не имеет решений')
+    if key == -1:
+        print("Система не имеет решений")
+        return
+    elif key == 0:
+        print("Система имеет бесконечно много решений")
+    else:
+        print("Система имеет единственное решение")
 
-        case 1:
-            for i in range(len(mat)):
-                for j in range(len(mat[i])-1):
-                    if mat[i][j] == 1:
-                        print(f'x{j+1} = {mat[i][-1]}')
+    n = len(mat)
+    m = len(mat[0]) - 1
+
+    for col in range(m):
+        for row in range(n):
+            if mat[row][col] != 0 and row == col:
+                break
+
+    for row in range(n):
+        for col in range(m):
+            if mat[row][col] == 1:
+                print(f'x{col + 1} = ', end='')
+                for j in range(m):
+                    if j != col and mat[row][j] != 0:
+                        if mat[row][j] < 0:
+                            print(f' + {abs(mat[row][j])}*x{j + 1}', end='')
+                        else:
+                            print(f' - {mat[row][j]}*x{j + 1}', end='')
+                if mat[row][m] != 0:
+                    if mat[row][m] < 0:
+                        print(f' {mat[row][m]}', end='')
+                    else:
+                        print(f' + {mat[row][m]}', end='')
+                print()
+                break
+        else:
+            if all(x == 0 for x in mat[row][:-1]):
+                if mat[row][m] != 0:
+                    print(f'0 = {mat[row][m]}')
+                else:
+                    print('0 = 0')
 
 
 def main():
     with open('lab1.txt', 'r') as file:
         l = [[int(num) for num in line.split(' ')] for line in file]
-    rank = np.linalg.matrix_rank(l)
-    n = -1
-    for i in range(len(l[0])):
-        n+=1
-    c = C(n,rank)
     fl,m = Jordan_Gauss(l)
     solve(fl,m)
 
