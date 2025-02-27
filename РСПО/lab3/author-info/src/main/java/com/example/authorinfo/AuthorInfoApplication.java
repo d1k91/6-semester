96ac7d2f58fbfe6ad5634380ed6fc13ba69c2e29
Vadim Glinskiy <v.glinskiy@.com>
2025-02-27 09:08:42 +0700

    Generate project on spring.io

---
+++ РСПО/lab3/author-info/src/main/java/com/example/authorinfo/AuthorInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorInfoApplication.class, args);
+	}
+
+}
